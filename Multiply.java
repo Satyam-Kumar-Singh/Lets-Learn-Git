dae6a5af32953bc624d7ee25c4644aa221380f2a
Satyam Kumar <satyamkumarsingh793@gmail.com>
2023-02-12 01:45:13 +0530

    adding multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+/**
+ * Multiply
+ */
+public class Multiply {
+
+    public static void main(String[] args) {
+        int a =3;
+        int b  = 4;
+        System.out.println("the multiplication of number is :"+(a*b));
+    }
+}
